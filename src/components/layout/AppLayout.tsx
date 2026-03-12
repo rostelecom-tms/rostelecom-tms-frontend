f2193696d0769f80fb1957024ac0d9865cf7a5a7
Edwin Denisov-Elers <edwindenisovelers9864@gmail.com>
2026-03-12 22:16:23 +0300

    initialize antd

---
+++ src/components/layout/AppLayout.tsx
@@ -0,0 +1,65 @@
+import React, {createElement, type FC} from 'react';
+import {
+    AppstoreOutlined,
+    BarChartOutlined,
+    CloudOutlined,
+    ShopOutlined,
+    TeamOutlined,
+    UploadOutlined,
+    UserOutlined,
+    VideoCameraOutlined,
+} from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Layout, Menu, theme } from 'antd';
+import { Outlet } from "react-router";
+
+const { Header, Content, Footer, Sider } = Layout;
+
+const siderStyle: React.CSSProperties = {
+    overflow: 'auto',
+    height: '100vh',
+    position: 'sticky',
+    insetInlineStart: 0,
+    top: 0,
+    scrollbarWidth: 'thin',
+    scrollbarGutter: 'stable',
+};
+
+const items: MenuProps['items'] = [
+    UserOutlined,
+    VideoCameraOutlined,
+    UploadOutlined,
+    BarChartOutlined,
+    CloudOutlined,
+    AppstoreOutlined,
+    TeamOutlined,
+    ShopOutlined,
+].map((icon, index) => ({
+    key: String(index + 1),
+    icon: createElement(icon),
+    label: `nav ${index + 1}`,
+}));
+
+export const AppLayout: FC = () => {
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
+
+    return (
+        <Layout hasSider>
+            <Sider style={siderStyle}>
+                <div className="demo-logo-vertical" />
+                <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
+            </Sider>
+            <Layout>
+                <Header style={{ padding: 0, background: colorBgContainer }} />
+                <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+                    <Outlet/>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>
+                    Ant Design ©{new Date().getFullYear()} Created by Ant UED
+                </Footer>
+            </Layout>
+        </Layout>
+    );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import {BrowserRouter} from "react-router";
+import {ConfigProvider} from "antd";
+import ruRu from "antd/locale/ru_RU";
+import {App} from "./App.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <BrowserRouter>
+          <ConfigProvider
+              locale={ruRu}
+              theme={{
+                  token: {
+                      colorPrimary: '#1677ff',
+                      borderRadius: 8,
+                  },
+              }}
+          >
+              <App />
+          </ConfigProvider>
+      </BrowserRouter>
   </StrictMode>,
 )
