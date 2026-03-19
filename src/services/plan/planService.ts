0d5a122d92ffe6e4c810bf093d8471aa3c04a69a
Edwin Denisov-Elers <edwindenisovelers9864@gmail.com>
2026-03-19 20:42:15 +0300

    plans logic + minor model fixes due to api changes

---
+++ src/services/plan/planService.ts
@@ -0,0 +1,35 @@
+import {api} from "../axios.ts";
+import type {IPlan, IPlanCreateRequest, IPlanUpdateRequest} from "../../models/plan/plan.ts";
+
+export default {
+    list: async (): Promise<IPlan[]> => {
+        const response = await api.get<IPlan[]>(`/plans`)
+        return response.data
+    },
+
+    get: async (id: number): Promise<IPlan[]> => {
+        const response = await api.get<IPlan[]>(`/plans/${id}`)
+        return response.data
+    },
+
+    create: async (id: number, request: IPlanCreateRequest): Promise<IPlan[]> => {
+        const response = await api.post<IPlan[]>(`/plans/${id}`, request)
+        return response.data
+    },
+
+    update: async (id: number, request: IPlanUpdateRequest): Promise<void> => {
+        await api.patch<void>(`/plans/${id}`, request)
+    },
+
+    delete: async (id: number): Promise<void> => {
+        await api.delete<void>(`/plans/${id}`)
+    },
+
+    addCase: async (planId: number, caseId: number): Promise<void> => {
+        await api.post<IPlan[]>(`/plans/${planId}/add-case/${caseId}`)
+    },
+
+    removeCase: async (planId: number, caseId: number): Promise<void> => {
+        await api.delete<void>(`/plans/${planId}/remove-case/${caseId}`)
+    }
+}
