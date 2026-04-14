ec47f98a98dfa7f2adfc1334733d4903c735d912
Edwin Denisov-Elers <edwindenisovelers9864@gmail.com>
2026-04-15 00:35:41 +0300

    defects

---
+++ src/services/case/defectService.ts
@@ -0,0 +1,20 @@
+﻿import {api} from "../axios.ts";
+import type {IDefect, IDefectCreateRequest, IDefectUpdateRequest} from "../../models/case/defect.ts";
+
+export default {
+    list: async (caseId?: number): Promise<IDefect[]> => {
+        const response = await api.get<IDefect[]>("/defects", {
+            params: { caseId },
+        });
+        return response.data;
+    },
+
+    create: async (request: IDefectCreateRequest): Promise<IDefect> => {
+        const response = await api.post<IDefect>("/defects", request);
+        return response.data;
+    },
+
+    update: async (id: number, request: IDefectUpdateRequest): Promise<void> => {
+        await api.patch<void>(`/defects/${id}`, request);
+    },
+};
